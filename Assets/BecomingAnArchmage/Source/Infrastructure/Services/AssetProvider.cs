355b4b2aeb961b9f6537e14338770255e7f9a595
quizandpuzzle <quizandpuzzle@gmail.com>
2023-08-17 20:09:02 +0300

    Create Asset Provider

---
+++ Assets/BecomingAnArchmage/Source/Infrastructure/Services/AssetProvider.cs
@@ -0,0 +1,17 @@
+//using Cysharp.Threading.Tasks;
+
+namespace BecomingAnArchmage.Source.Infrastructure.Services
+{
+    public interface IAssetProvider
+    {
+       // UniTask Initialize();
+    }
+
+    public class AssetProvider : IAssetProvider
+    {
+        //public async UniTask Initialize()
+        //{
+        //    await UniTask.Yield();
+        //}
+    }
+}
